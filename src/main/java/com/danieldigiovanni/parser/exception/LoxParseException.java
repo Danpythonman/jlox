129c0fd22629c0c884cf9df3ad0d37648bc74c82
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-05-08 17:53:31 -0400

    Added documentation for readability in LoxParseException.

--- src/main/java/com/danieldigiovanni/parser/exception/LoxParseException.java
+++ src/main/java/com/danieldigiovanni/parser/exception/LoxParseException.java
@@ -1,17 +1,29 @@
 package com.danieldigiovanni.parser.exception;
 
 import com.danieldigiovanni.token.Token;
+import lombok.Getter;
 
+/**
+ * Exception representing an exception when parsing Lox code.
+ */
+@Getter
 public class LoxParseException extends RuntimeException {
 
+    /**
+     * The token that caused the exception.
+     */
     private final Token token;
 
+    /**
+     * Construct a {@link LoxParseException} with the token that caused the
+     * exception and an error message.
+     *
+     * @param token The token that caused the exception.
+     * @param message The error message.
+     */
     public LoxParseException(Token token, String message) {
         super(message);
         this.token = token;
     }
 
-    public Token getToken() {
-        return this.token;
-    }
 }
